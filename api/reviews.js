90443001cd1f8e83962f4461179e20d3a4dca8aa
isabelleadrury <isabelledrury38@gmail.com>
2025-12-15 20:08:38 -0500

    updated schema

--- api/reviews.js
+++ api/reviews.js
@@ -1,13 +1,36 @@
 import express from "express";
+import requireBody from "#middleware/requireBody";
+import requireUser from "#middleware/requireUser";
+
 const router = express.Router();
 export default router;
 
-import { getReviewsByUserId } from "#db/queries/reviews";
+import { getReviewsByUserId, addReview } from "#db/queries/reviews";
+
+router.route("/:id/reviews").get(async (req, res) => {
+  const { page = 1, limit = 3 } = req.query;
+  const reviews = await getReviewsByUserId(
+    req.params.id,
+    parseInt(page),
+    parseInt(limit)
+  );
+  return res.json(reviews);
+});
 
 router
-  .route("/:id/reviews")
-  .get(async (req, res) => {
-    const { page = 1, limit = 3 } = req.query;
-    const reviews = await getReviewsByUserId(req.params.id, parseInt(page), parseInt(limit));
-    return res.json(reviews);
-  });
+  .route("/:hotelId/reviews")
+  .post(
+    requireUser,
+    requireBody(["rating", "subject", "review"]),
+    async (req, res) => {
+      const { rating, subject, review } = req.body;
+      const userReview = await addReview(
+        req.params.hotelId,
+        req.user.id,
+        rating,
+        subject,
+        review
+      );
+      res.status(201).json(userReview);
+    }
+  );
