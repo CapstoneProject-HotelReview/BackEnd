df0022ed44a6f2e784116bdf22c443cf45f5dcad
SHussain47 <hussain.syed.6732@gmail.com>
2026-03-24 20:04:03 -0400

    fix: fixed review next button code

--- db/queries/reviews.js
+++ db/queries/reviews.js
@@ -1,23 +1,6 @@
 import db from "#db/client";
 
 /* Retrieve reviews from user - latest first and 3 at a time */
-// export async function getReviewsByUserId(userId, page = 1, limit = 3) {
-//   const offset = (page - 1) * limit;
-//   try {
-//     const sql = `
-//       SELECT * FROM reviews
-//       WHERE user_id = $1
-//       ORDER BY created_at DESC
-//       LIMIT $2 OFFSET $3
-//     `;
-//     const values = [userId, limit, offset];
-//     const { rows: reviews } = await db.query(sql, values);
-//     return reviews;
-//   } catch (error) {
-//     console.error("Error with getReviewsByUserId query: ", error);
-//     throw error;
-//   }
-// }
 export async function getReviewsByUserId(userId, page = 1, limit = 3) {
   const offset = (page - 1) * limit;
 
